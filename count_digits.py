4fa738b0c85a4de138de2a3d854c2deb8e65aa23
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 13:46:41 -0700

    Update count_digits.py

--- count_digits.py
+++ count_digits.py
@@ -4,10 +4,15 @@
 #Unit 8 Labs
 #Count Digits
 
+#Get integer from user
 num = int(input("Enter an integer: "))
+#Set count equal to 0
 count = 0
+#While num is greater than 0
 while (num > 0):
+    #Adds one to count for each digit
     num = num // 10
     count = count + 1
 
+#Print the number of digits 
 print(count)
